45e1271c450ff09d3aad23b30ee6b701c7d7a332
James <jamesh21@uw.edu>
2017-03-05 05:05:29 -0800

    Fix async task with the gyms on the home page

--- app/src/main/java/group2/tcss450/uw/edu/gymwatch/data/LoginSavePreference.java
+++ app/src/main/java/group2/tcss450/uw/edu/gymwatch/data/LoginSavePreference.java
@@ -7,16 +7,31 @@ import android.content.SharedPreferences;
  * Created by James on 3/5/2017.
  */
 
+/**
+ * This class is used for saving the username to the shared preference, in order for auto login.
+ */
 public class LoginSavePreference {
 
+    /** Constant for the key of the shared preference. */
     private static final String PREF_USER = "username";
+    /** Constant for the name of the shared preference file. */
     private static final String PREF_FILE = "group2.tcss450.uw.edu.gymwatch_preferences";
 
+    /**
+     * This method is used to set the username within the shared preference file.
+     * @param context of the application
+     * @param username the new username to set to the file
+     */
     public static void setUser(Context context, String username) {
         SharedPreferences prefs = context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
         prefs.edit().putString(PREF_USER, username).apply();
     }
 
+    /**
+     * This method is used to get the username within the shared preference file.
+     * @param context of the application
+     * @return the username within the file
+     */
     public static String getUser(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
         return prefs.getString(PREF_USER, "");
--- app/src/main/java/group2/tcss450/uw/edu/gymwatch/fragments/MyGymsFragment.java
+++ app/src/main/java/group2/tcss450/uw/edu/gymwatch/fragments/MyGymsFragment.java
@@ -1,28 +1,63 @@
 package group2.tcss450.uw.edu.gymwatch.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import group2.tcss450.uw.edu.gymwatch.R;
 import group2.tcss450.uw.edu.gymwatch.data.GymAdapter;
 import group2.tcss450.uw.edu.gymwatch.data.GymItem;
-import group2.tcss450.uw.edu.gymwatch.data.GymListData;
 
 
 /**
  * This class is for the myGyms fragment which will hold gyms added by the user.
  */
 public class MyGymsFragment extends Fragment {
+
+    private static final String PARTIAL_URL
+            = "http://cssgate.insttech.washington.edu/~xufang/getGymsFromDB.php";
+    /** The URL for getting google images. */
+    private static String IMAGE_URL = "https://maps.googleapis.com/maps/api/place" +
+            "/photo?maxwidth=400&photoreference=";
+
+    /** API KEY for this app. */
+    private static String API_KEY = "&key=AIzaSyC32qpLF5AVQGXBEq0iCGkCHHAI9V8Eb1w";
+
+    private static final String FAKE_DATA = "http://cssgate.insttech.washington.edu/~xufang/" +
+            "fakeData.php";
+    /** This URL is used for when there are no pictures available for that gym. */
+
+    private static final String NO_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/" +
+            "thumb/a/ac/No_image_available.svg/2000px-No_image_available.svg.png";
+
+    private static final String TAG_NAME = "gymname";
+    private static final String TAG_IMAGE = "imageurl";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_RATING = "rating";
+    private static List<GymItem> mUserGyms;
+
+    private View mView;
 
     @Override
     /**
@@ -31,22 +66,144 @@ public class MyGymsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Log.d("Creating Gyms Fragment", "!!!");
         String username = getArguments().getString("username");
-        System.out.println("This is the username" + username);
-        View view = inflater.inflate(R.layout.fragment_my_gyms, container, false);
-        RecyclerView gymRecView = (RecyclerView) view.findViewById(R.id.gym_home_list);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        gymRecView.setLayoutManager(layoutManager);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration
-                (gymRecView.getContext(), layoutManager.getOrientation());
-        gymRecView.addItemDecoration(dividerItemDecoration);
-        List<GymItem> gymList = new ArrayList<>();
-        gymList = GymListData.getList(username);
-        GymAdapter gymAdapter = new GymAdapter(gymList, getActivity());
-        System.out.println(gymList + "!!!!!!!!!!!!!!!");
-        gymRecView.setAdapter(gymAdapter);
-        return view;
+        mView = inflater.inflate(R.layout.fragment_my_gyms, container, false);
+
+        mUserGyms = new ArrayList<>();
+        AsyncTask<String, Void, String> task = new GetGymsFromDBTask();
+        task.execute(PARTIAL_URL, username);
+        return mView;
+    }
+
+    /**
+     * Inner AsyncTask class, handle the internet connection with the web server. Passing
+     * parameters to the web server and get the response back.
+     */
+    private class GetGymsFromDBTask extends AsyncTask<String, Void, String> {
+
+        /**
+         * perform web connection, passing parameters and retrieve response back by POST
+         * @param strings includes destination URL, and parameters we want to pass.
+         * @return response is the string we get back from the web server.
+         */
+        @Override
+        protected String doInBackground(String... strings) {
+            if (strings.length != 2) {
+                throw new IllegalArgumentException("Two String arguments required.");
+            }
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            String url = strings[0];
+            try {
+                URL urlObject = new URL(url);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                String data = URLEncoder.encode("username", "UTF-8")
+                        + "=" + URLEncoder.encode(strings[1], "UTF-8");
+                wr.write(data);
+                wr.flush();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                response = "Unable to connect, Reason: "
+                        + e.getMessage();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return response;
+        }
+
+        /**
+         * This method checks if the response is valid, If it is, store the response
+         * in to the field mResponse.
+         * @param result the response string we  get back from the server.
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray json = new JSONArray(result);
+                List<GymItem> userGyms = new ArrayList<>();
+                for (int i = 0; i < json.length(); i++) {
+                    String str_result = new StaticWebServiceTask().execute(FAKE_DATA).get();
+                    JSONObject gym = json.getJSONObject(i);
+                    String gymName = gym.getString(TAG_NAME);
+                    String gymAddress = gym.getString(TAG_ADDRESS);
+                    String photoReference = gym.getString(TAG_IMAGE);
+                    String gymImage;
+                    if (!photoReference.isEmpty()) {
+                        gymImage = IMAGE_URL + photoReference + API_KEY;
+                    } else {
+                        gymImage = NO_IMAGE;
+                    }
+                    String gymRating = gym.getString(TAG_RATING);
+                    GymItem currentGym = new GymItem(gymName, gymRating, gymAddress, str_result, gymImage);
+                    userGyms.add(currentGym);
+                }
+                RecyclerView gymRecView = (RecyclerView) mView.findViewById(R.id.gym_home_list);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                gymRecView.setLayoutManager(layoutManager);
+                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration
+                        (gymRecView.getContext(), layoutManager.getOrientation());
+                gymRecView.addItemDecoration(dividerItemDecoration);
+                GymAdapter gymAdapter = new GymAdapter(userGyms, getActivity());
+                gymRecView.setAdapter(gymAdapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+    /**
+     * Inner AsyncTask class, handle the internet connection with the web server. Passing
+     * parameters to the web server and get the response back.
+     */
+    private class StaticWebServiceTask extends AsyncTask<String, Void, String> {
+
+        /**
+         * Perform web connection, passing parameters and retrieve response back by POST
+         * @param strings includes destination URL, and parameters we want to pass.
+         * @return response is the string we get back from the web server.
+         */
+        @Override
+        protected String doInBackground(String... strings) {
+            if (strings.length != 1) {
+                throw new IllegalArgumentException("One String arguments required.");
+            }
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            String url = strings[0];
+            try {
+                URL urlObject = new URL(url);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                response = "Unable to connect, Reason: "
+                        + e.getMessage();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return response;
+        }
+
     }
 
 }
